ad2ac05b3e94c001810b72742a9ed7174a873f3c
SergeyKouzmin <KouzminSergey@mail.ru>
2015-05-15 18:34:53 +0300

    wwwww

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace std;
 
@@ -13,7 +16,8 @@ int main() {
 
     cv::namedWindow("CameraVideo",CV_WINDOW_AUTOSIZE);
 
-    while (1) {
+   /* int i = 0;
+    while (i < 100) {
         cv::Mat frame;
 
         if (!camera.read(frame)) {
@@ -22,12 +26,43 @@ int main() {
         }
 
         cv::imshow("CameraVideo", frame);
+        std::stringstream fileName;
+        fileName << "/home/sergey/Programs/projects/object_detect/object_detect/images/" << i << ".jpg";
+        cv::imwrite(fileName.str(), frame);
 
-        if (cv::waitKey(30) == 27) {
-                cout << "esc key is pressed by user" << endl;
-                break;
-        }
-    }
+        i++;
+    }*/
+
+    cv::Mat image = cv::imread("/home/sergey/Programs/projects/object_detect/object_detect/images/24.jpg");
+    cv::Mat image_gray;
+    cv:cvtColor( image, image_gray, CV_BGR2GRAY );
+    cv::threshold(image_gray, image_gray, 115, 255, CV_THRESH_BINARY);
+
+    cv::Mat canny_output;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    std::vector<cv::Rect> boundRect( contours.size() );
+
+    cv::Canny( image_gray, canny_output, 30, 60, 3 );
+     /// Find contours
+    cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+
+
+    std::vector<std::vector<cv::Point> > contours_poly(contours.size());
+
+    /*for( int i = 0; i < contours.size(); i++ ) {
+        cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
+        boundRect[i] = cv::boundingRect( cv::Mat(contours_poly[i]) );
+    }*/
+
+    cv::Mat drawing = cv::Mat::zeros( canny_output.size(), CV_8UC3 );
+    /*for( int i = 0; i< contours.size(); i++ ) {
+        cv::Scalar color = cv::Scalar( 255, 0, 0);
+        //cv::drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
+        cv::rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+    }*/
+
+    cv::imwrite("/home/sergey/Programs/projects/object_detect/object_detect/result.jpg", drawing);
 
     return 0;
 
