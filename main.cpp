7e4b01e5f67f37200de5805ba6620b3e447aa871
SergeyKouzmin <KouzminSergey@mail.ru>
2015-05-15 18:58:34 +0300

    add find rect

--- main.cpp
+++ main.cpp
@@ -33,36 +33,54 @@ int main() {
         i++;
     }*/
 
-    cv::Mat image = cv::imread("/home/sergey/Programs/projects/object_detect/object_detect/images/24.jpg");
+    /*cv::Mat image = cv::imread("/home/sergey/Programs/projects/object_detect/object_detect/images/24.jpg");
     cv::Mat image_gray;
     cv:cvtColor( image, image_gray, CV_BGR2GRAY );
-    cv::threshold(image_gray, image_gray, 115, 255, CV_THRESH_BINARY);
+    cv::threshold(image_gray, image_gray, 115, 255, CV_THRESH_BINARY);*/
 
     cv::Mat canny_output;
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
     std::vector<cv::Rect> boundRect( contours.size() );
 
-    cv::Canny( image_gray, canny_output, 30, 60, 3 );
-     /// Find contours
-    cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    for (int i = 24; i < 100; i++) {
+        std::stringstream fileName;
+        fileName << "/home/sergey/Programs/projects/object_detect/object_detect/images/" << i << ".jpg";
+
+        cv::Mat image = cv::imread(fileName.str());
+        cv::Mat image_gray;
+
+        cv:cvtColor( image, image_gray, CV_BGR2GRAY );
+        cv::threshold(image_gray, image_gray, 115, 255, CV_THRESH_BINARY);
+
+        cv::Canny( image_gray, canny_output, 30, 60, 3 );
+        /// Find contours
+        cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 
 
-    std::vector<std::vector<cv::Point> > contours_poly(contours.size());
+        std::vector<std::vector<cv::Point> > contours_poly(contours.size());
 
-    /*for( int i = 0; i < contours.size(); i++ ) {
+        /*for( int i = 0; i < contours.size(); i++ ) {
         cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
         boundRect[i] = cv::boundingRect( cv::Mat(contours_poly[i]) );
-    }*/
+        }*/
 
-    cv::Mat drawing = cv::Mat::zeros( canny_output.size(), CV_8UC3 );
-    /*for( int i = 0; i< contours.size(); i++ ) {
-        cv::Scalar color = cv::Scalar( 255, 0, 0);
-        //cv::drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
-        cv::rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-    }*/
+        cv::Mat drawing = cv::Mat::zeros( canny_output.size(), CV_8UC3 );
+        for( int i = 0; i< contours.size(); i++ ) {
+            cv::Scalar color = cv::Scalar( 255, 0, 0);
+            cv::Rect boundRect = cv::boundingRect( cv::Mat(contours[i]) );
+            //cv::drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
+            //cv::rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+            if (boundRect.area() > 4000) {
+                cv::rectangle(drawing, boundRect, color);
+            }
+        }
 
-    cv::imwrite("/home/sergey/Programs/projects/object_detect/object_detect/result.jpg", drawing);
+        std::stringstream saveFrame;
+        saveFrame << "/home/sergey/Programs/projects/object_detect/object_detect/image_gray/" << i << ".jpg";
+
+        cv::imwrite(saveFrame.str(), drawing);
+    }
 
     return 0;
 
